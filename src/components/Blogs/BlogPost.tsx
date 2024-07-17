1eebd5fc98f1ae7b96f101d1b626d1b128870c10
Bidisha Mondal <mondal.bidisha@gmail.com>
2024-07-17 19:56:42 +0530

    added back button for blog post page

--- src/components/Blogs/BlogPost.tsx
+++ src/components/Blogs/BlogPost.tsx
@@ -5,6 +5,8 @@ import { Loader } from '../Loader';
 import Meteors from '../magicui/meteors';
 import { useBlog } from '@/hooks';
 import generateLoadingMessage from '@/utils/genericUtils';
+import Link from 'next/link';
+import { IoChevronBackCircleOutline } from 'react-icons/io5';
 
 const BlogPost = (props: any) => {
   const { id } = props;
@@ -24,12 +26,19 @@ const BlogPost = (props: any) => {
   }
 
   return (
-    <div className="relative min-h-screen flex flex-col justify-center items-center p-4 md:p-20 bg-gradient-to-b from-slate-950 via-indigo-950 to-slate-800">
+    <div className="md:p-20 w-full flex flex-col bg-gradient-to-b from-slate-950 via-indigo-950 to-slate-800">
       <Meteors number={50}/>
-      <div className="p-4 max-w-[70%]">
-        <div className="text-xl md:text-5xl font-extrabold py-4 line-clamp-4 text-white">{blog?.title}</div>
-        <div className="py-4 text-slate-200 text-xl full-blog-post-preview">
-          <ReactQuill value={blog?.content} readOnly={true} theme={'bubble'} />
+      <div className="w-10 h-10 flex items-center justify-center">
+        <Link href={'/blog'}>
+            <IoChevronBackCircleOutline className="text-4xl text-white cursor-pointer" />
+        </Link>
+      </div>
+      <div className="relative min-h-screen flex flex-col justify-center items-center p-4">
+        <div className="p-4 max-w-[70%]">
+          <div className="text-xl md:text-5xl font-extrabold py-4 line-clamp-4 text-white">{blog?.title}</div>
+          <div className="py-4 text-slate-200 text-xl full-blog-post-preview">
+            <ReactQuill value={blog?.content} readOnly={true} theme={'bubble'} />
+          </div>
         </div>
       </div>
     </div>
--- src/components/GetInvolved/Landing.tsx
+++ src/components/GetInvolved/Landing.tsx
@@ -25,9 +25,6 @@ function Landing() {
 				<p className="flex items-center text-justify my-5 flex-col text-slate-100 gap-2 text-3xl px-10">
 					A LOT !!! 
 				</p>
-				{/* <div className="flex items-center text-justify flex-col text-slate-100 gap-2 text-xl px-10">
-					Living sustainably also has the potential to improve health and well-being for all.
-				</div> */}
 				<div className="flex items-center mt-10 mb-10 flex-col text-slate-100 gap-2 text-3xl font-medium uppercase opacity-90 tracking-[4px]">
 					Here’s where each one of us can make a difference
 				</div>
